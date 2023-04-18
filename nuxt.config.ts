af0693296565251188f618e9a589e02be9baabba
Sanni Emmanuel <sanniemmanuel@macbooks-MacBook-Pro.local>
2023-04-18 10:19:06 +0100

    added nuxt apollo config and morty api link

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    modules: ['@nuxtjs/apollo'],
 
+    apollo: {
+          clients: {
+            default: {
+              httpEndpoint: 'https://rickandmortyapi.com/graphql'
+        }
+      },
+    },
 })
